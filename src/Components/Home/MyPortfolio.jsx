c43fd863e2da3771525970d19d480b05ac012435
Snir4game <sniramsalem1995@gmail.com>
2024-11-13 01:54:38 +0200

    Skill Proficiency

--- src/Components/Home/MyPortfolio.jsx
+++ src/Components/Home/MyPortfolio.jsx
@@ -5,12 +5,12 @@ import GmailPic from '../Pics/Gmail.png';
 import GitHubPic from '../Pics/GitHub.png';
 import Selfie from '../Pics/Selfie.png';
 const MyPortfolio = () => {
+  
   return (
-    <div>
+    <div className='container-fluid'>
       <div className="hero">
         <h2>Snir Amsalem</h2>
         <h4>Full Stack Developer</h4>
-        <img className='Selfie' src={Selfie} alt="" />
         <div className ="Social-Media">
           <a href ="https://www.linkedin.com/in/sniramsalem/">
             <img  src = {LinkdinPic} alt="" />
@@ -22,6 +22,7 @@ const MyPortfolio = () => {
             <img src = {GitHubPic} alt="" />
           </a>
         </div>
+          <img className='Selfie' src={Selfie} alt="" />
       </div>
       <div className="about">
         <p>
@@ -31,8 +32,41 @@ const MyPortfolio = () => {
         Seeking to leverage software development skills in a challenging Full Stack Developer role. <br />
         </p>
       </div>
-      <div className="skills">
-
+      <div className="container mt-1">
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Skill</th>
+              <th>Proficiency</th>
+            </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>JavaScrip</th>
+              </tr>
+              <tr>
+                <th>HTML</th>
+              </tr>
+              <tr>
+                <th>CSS</th>
+              </tr>
+              <tr>
+                <th>React</th>
+              </tr>
+              <tr>
+                <th>React-Native</th>
+              </tr>
+              <tr>
+                <th>FireBase</th>
+              </tr>
+              <tr>
+                <th>BootStrap</th>
+              </tr>
+              <tr>
+                <th></th>
+              </tr>
+            </tbody>
+        </table>
       </div>
       <div className="projects">
 
